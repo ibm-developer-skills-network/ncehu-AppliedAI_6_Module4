a2ff661fd50fa00b621eae3b2e97203c1cbb3f3b
lavskillup <lavanyas@skillup.tech>
2022-09-08 11:28:18 +0530

    Create db2_connectapis.py

---
+++ DB2Connectivity/db2_connectapis.py
@@ -0,0 +1,60 @@
+import ibm_db
+import os
+import re
+# import json 
+from flask import Flask
+from flask import request
+from flask import jsonify 
+import json
+app = Flask(__name__) 
+dsn_hostname = os.environ['HOST_URL']
+dsn_uid = os.environ['USERNAME']
+dsn_pwd = os.environ['PASSWORD']
+dsn_port = os.environ['PORT']
+dsn_database = "bludb"            
+dsn_driver = "{IBM DB2 ODBC DRIVER}"
+dsn_protocol = "TCPIP"            
+dsn_security = "SSL"              
+#Create the dsn connection string
+dsn = (
+    "DRIVER={0};"
+    "DATABASE={1};"
+    "HOSTNAME={2};"
+    "PORT={3};"
+    "PROTOCOL={4};"
+    "UID={5};"
+    "PWD={6};"
+    "SECURITY={7};").format(dsn_driver, dsn_database, dsn_hostname, dsn_port, dsn_protocol, dsn_uid, dsn_pwd, dsn_security)
+# #Create database connection
+try:
+    conn = ibm_db.connect(dsn, "", "")
+    print ("Connected to database: ", dsn_database, "as user: ", dsn_uid, "on host: ", dsn_hostname)
+    command = "select * from test where LOWER(NAME) like '%{}%' OR LOWER(DESCRIPTION) like '%{}%' ".format("arg","arg") 
+except:
+    print ("Unable to connect: ", ibm_db.conn_errormsg() )
+@app.route('/courses', methods = ['POST','GET'])  
+def course_details(): 
+  user_data = request.get_json()  
+  print(user_data['name']) 
+  lis = list(user_data['name'].split(" "))
+
+  # length of list
+  length = len(lis)
+
+  # returning last element in list
+  arg=lis[length-1]
+#   arg=user_data['name']
+  
+  
+
+  if arg is not None: 
+    arg = arg.lower() 
+    command = "select * from test1 where LOWER(NAME) like '%{}%' ".format(arg,arg) 
+    print(command)
+    stmt = ibm_db.exec_immediate(conn, command)
+    dictionary = ibm_db.fetch_both(stmt)
+    coursename = dictionary['NAME']
+    desc = dictionary['DESCRIPTION'] 
+    ret_val = '{"Name":"%s","Description":"%s"}'%(coursename,desc)
+    return json.loads(ret_val)
+app.run(host ='0.0.0.0', port = 5000, debug = True)
